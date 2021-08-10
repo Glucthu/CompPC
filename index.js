25b02296446f8a5ca4fd1b7d3efc298ec399f9a9
Glucthu <glucthu@protonmail.com>
2021-08-10 14:46:17 -0300

    added registered page

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function initMap() {
-    const uluru = { lat: -34.70, lng: -58.58 };
+    const arg = { lat: -34.70, lng: -58.58 };
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 4,
-      center: uluru,
+      center: arg,
     });
 }
 
